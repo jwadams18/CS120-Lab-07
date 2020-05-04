ffff60e79afd49eb08799bdb0a01c97306b688d6
Jadams0 <59323501+Jadams0@users.noreply.github.com>
2020-05-04 00:04:01 -0500

    Initial Commit

---
+++ src/Starter.java
@@ -0,0 +1,15 @@
+/*
+        James Adams
+        Lab-07-jadams18
+        Starter.java
+        @username jwadams18
+         */
+
+public class Starter {
+
+    public static void main(String[] args) {
+
+
+        Classifier videoGames = new Classifier();
+    }
+}
